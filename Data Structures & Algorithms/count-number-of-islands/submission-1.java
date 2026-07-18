819a8f85b7416e94f25ed5cd673e3f122e32d089
yash-shinde <61104860+yash-shinde@users.noreply.github.com>
2026-07-18 21:24:14 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    private static final int[][] directions = 
+    {{1, 0}, {-1, 0},{0, 1}, {0, -1}};
+
+    public int numIslands(char[][] grid) {
+        //basic logic - we mark all the visited cells as water and 
+        //go ahead.in order to not count the same cells again
+        //for all '1' cells , we perform dfs and set its value as 0(water)
+        //then we check al adjacent 8 cells - whichever are valid
+        //if any is 0 we return 
+        //if 1 we dfs again
+
+        int ROWS = grid.length;int COLS = grid[0].length;
+        int islands = 0;
+
+        for(int i=0;i<ROWS;i++){
+            for(int j=0;j<COLS;j++){
+                if(grid[i][j] == '1'){
+                    islands+=1;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    public void dfs(char[][] grid,int r,int c){
+        if(r < 0 || r >= grid.length || 
+        c < 0 || c >= grid[0].length || grid[r][c] == '0'){
+            return;
+        }
+        //mark visited as water
+        grid[r][c] = '0';
+
+        for(int[] dir : directions){
+            dfs(grid,r+dir[0],c+dir[1]);
+        }
+    }
+}
